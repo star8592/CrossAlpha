26f9fbb3866513f498672d49c7cb26c6683155be
star8592 <yang-mian@hotmail.com>
2026-09-06 00:26:21 +0800

    Stabilize Observatory module exports for rustfmt

--- crates/crossalpha-observatory/src/lib.rs
+++ crates/crossalpha-observatory/src/lib.rs
@@ -1,10 +1,15 @@
 pub mod health;
 pub mod providers;
 
-pub use health::{
-    load_manifest, observatory_health, verify_snapshot, write_health_report, ObservatoryHealthReport,
-    SeriesHealth, DEFAULT_EXPECTED_SERIES,
-};
-pub use providers::{
-    parse_sources, ProviderClient, ProviderSource, DEFILLAMA_STABLECOINS_URL, HYPERLIQUID_URL,
-};
+pub use health::DEFAULT_EXPECTED_SERIES;
+pub use health::ObservatoryHealthReport;
+pub use health::SeriesHealth;
+pub use health::load_manifest;
+pub use health::observatory_health;
+pub use health::verify_snapshot;
+pub use health::write_health_report;
+pub use providers::DEFILLAMA_STABLECOINS_URL;
+pub use providers::HYPERLIQUID_URL;
+pub use providers::ProviderClient;
+pub use providers::ProviderSource;
+pub use providers::parse_sources;
